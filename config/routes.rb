f8f302531589c8073357fd353669a1078a739bb1
nakachan1994 <nakachaaaan@gmail.com>
2021-04-14 00:45:41 +0000

    [finish]MVCの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  devise_for :users
   root to:'homes#top'
   get 'homes/about'
+  devise_for :users
+  resources :books, only:[:new, :create, :index, :show, :destroy]
+  resources :users, only:[:index, :show, :edit]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
