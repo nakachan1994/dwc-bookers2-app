9e75dbc9d3b68262bc39a010934075f0b10b45c4
nakachan1994 <nakachaaaan@gmail.com>
2021-04-15 13:39:41 +0000

    [update]users/editビュー

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'homes/about'
   devise_for :users
   resources :books, only:[:new, :create, :index, :show, :edit,:destroy, :update]
-  resources :users, only:[:index, :show, :edit]
+  resources :users, only:[:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
