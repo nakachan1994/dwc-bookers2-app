c65ef21ead9231fac849d9f49c688ab6f27a3e57
nakachan1994 <nakachaaaan@gmail.com>
2021-04-17 02:06:42 +0000

    [update]バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   attachment :profile_image
 
   validates :name,presence:true, uniqueness:true, length:{minimum:2,maximum:20}
+  validates :introduction, length:{maximum:50}
 end
