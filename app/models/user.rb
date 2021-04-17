f8faecab4b47ff521fd6733ef34d35fddf6e0634
nakachan1994 <nakachaaaan@gmail.com>
2021-04-17 01:30:58 +0000

    [add]ユーザー更新バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   attachment :profile_image
+
+  validates :name,presence:true, uniqueness:true, length:{minimum:2,maximum:20}
 end
