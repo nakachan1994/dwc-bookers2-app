392754a6b2914e1d762f80ec2aaf0a6f26966000
nakachan1994 <nakachaaaan@gmail.com>
2021-04-17 04:36:37 +0000

    [update]ログイン時ヘッダーHomeリンク先

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id), notice: 'You have created book succesfully.'
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
       @user = current_user
       @books = Book.all
@@ -31,6 +31,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @user = current_user
     if @book.user == current_user
       render :edit
     else
@@ -41,8 +42,9 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id), notice: 'You have updated book succesfully.'
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
     else
+      @user = current_user
       render :edit
     end
   end
