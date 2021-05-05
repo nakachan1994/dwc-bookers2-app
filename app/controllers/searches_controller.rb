f0b3ffc7152d52b23fba024f3b6cb7af38824322
nakachan1994 <nakachaaaan@gmail.com>
2021-05-05 06:28:45 +0000

    [update]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,11 @@
 class SearchesController < ApplicationController
   def search
     @range = params[:range]
-    if @range == "USer"
+    if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
     end
+    @user = current_user
   end
 end
