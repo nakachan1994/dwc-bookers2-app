ba6403feeb39e0331e57c5430f10941dbb777123
nakachan1994 <nakachaaaan@gmail.com>
2021-05-06 07:36:12 +0000

    [add]コメント非同期エラーメッセージ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,14 +4,13 @@ class BookCommentsController < ApplicationController
     @book_comment = BookComment.new(book_comment_params)
     @book_comment.user_id = current_user.id
     @book_comment.book_id = @book.id
-    unless @book_comment.save
-     render 'errors'
-    end
+    @book_comment.save
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:book_id])
   end
 
   private
